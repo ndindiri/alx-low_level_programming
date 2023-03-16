616c7c38399fa60c56d5d9a41d875135646090de
ndindiri <ndbut5@gmail.com>
2023-03-16 12:44:06 -0700

    Task 4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ * Return: always return 0 if a instructions are successfull
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
