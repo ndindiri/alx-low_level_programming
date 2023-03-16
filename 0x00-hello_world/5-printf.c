05551b110843e0c06c01f60cd000b3e6cfdc2d5f
ndindiri <ndbut5@gmail.com>
2023-03-16 12:56:27 -0700

    Task 5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -this is the  main function
+ * Return: retun 0 if instructions are successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
