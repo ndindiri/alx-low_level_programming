6e3b45d383a1e849cb10db611bafa3cfd3bbe556
ndindiri <ndbut5@gmail.com>
2023-03-17 13:59:18 -0700

    Task 6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print all single digit numbers of base 10 starting from
+ * You are not allowed to use any variable of type char
+ * You can only use the putchar function
+ * Return: should return 0
+ */
+int main(void)
+{
+	int nbr = 48;
+
+	while (nbr <= 57)
+	{
+		putchar(nbr);
+		nbr += 1;
+	}
+	printf("\n");
+	return (0);
+}
