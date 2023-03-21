4eb28a9aede653819f6fa7b27d0ecffc9c54feb9
ndindiri <ndbut5@gmail.com>
2023-03-21 12:28:49 -0700

    Task 0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main- function to print _putchar
+ * _putchar: print alphabets of _putchar
+ * Return: should return 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+
+}
