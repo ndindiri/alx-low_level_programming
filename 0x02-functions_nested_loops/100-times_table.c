233f815c7cc52570beb907b04c5e7152947bd030
ndindiri <ndbut5@gmail.com>
2023-03-21 15:50:56 -0700

    Task 12. The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - function that adds two integers and returns the result.
+ * @nbr1: input integer 1
+ * @nbr2: input integer 2
+ * Return: should return result
+ */
+
+int add(int nbr1, int nbr2)
+{
+	int result;
+
+	result = nbr1 + nbr2;
+	return (result);
+}
