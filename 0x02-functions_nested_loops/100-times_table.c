832c31c28cbf95072084d6ad8f2e7ff4beed443e
ndindiri <ndbut5@gmail.com>
2023-03-22 00:36:06 -0700

    Task 12. The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,15 +1,49 @@
 #include "main.h"
 /**
- * add - function that adds two integers and returns the result.
- * @nbr1: input integer 1
- * @nbr2: input integer 2
+ * print_times_table - function that adds two integers and returns the result.
+ * @n: input to checkk
+ * If n >= 15 || <= 0 the function should not print anything
  * Return: should return result
  */
-
-int add(int nbr1, int nbr2)
+void print_times_table(int n)
 {
-	int result;
+	int x, y, z;
 
-	result = nbr1 + nbr2;
-	return (result);
-}
+	if (n >= 0 && n <= 14)
+	{
+		for (x = 0; x <= n; x++)
+		{
+			for (y = 0; y <= n; y++)
+			{
+				z = x * y;
+
+				if (z > 99)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else if (z > 9)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else
+				{
+					if (y != 0)
+					{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(32);
+					}
+					_putchar(z + '0');
+				}
+			}
+			_putchar('\n');
+		}}}
